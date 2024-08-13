1213087623e9056f66f6d5966ced8cf9e2895d7f
linuxct <linuxct@linuxct.space>
2024-08-13 16:42:31 +0200

    feat: Add branding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "space.linuxct.hydra"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     buildTypes {
